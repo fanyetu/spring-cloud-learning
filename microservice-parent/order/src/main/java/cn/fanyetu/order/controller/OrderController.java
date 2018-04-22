52146565b68522784df6575f6f7573be7af8b4db
fanyetu <hnan187@126.com>
2018-04-22 23:07:53 +0800

    commit

---
+++ microservice-parent/order/src/main/java/cn/fanyetu/order/controller/OrderController.java
@@ -0,0 +1,15 @@
+package cn.fanyetu.order.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhanghaonan
+ * @date 2018/4/19
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+
+}
